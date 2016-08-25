7238ae571471d0aca43077a232b1ecb445cdb107
tejbans <singh.tejbans@gmail.com>
2016-08-25 13:51:35 -0700

    Added task_controller.rb

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,48 @@
+module Api
+ module V1
+  class TasksController < ApplicationController
+    ship_before_filter :verify_authenticity_token
+    respond_to :json
+
+    def index
+      respond_with(Note.all)
+    end
+
+    def show
+      respond_with(Task.find(params[:id]))
+    end
+
+    def create
+      @task = Task.new(task_params)
+      if @task.save
+        respond_to do |format|
+          format.json {render :json => @task}
+        end
+      end
+    end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      respond_to do |format|
+        format.json {render :json => @task}
+      end
+    end
+  end
+
+  def destroy
+    respond_with  Task.destroy(params[:id])
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:title, :description, :due_date, :status)
+    end
+
+  end
+end
+end
+
+
+
+

c138ee39a1a29133564cdda3e804d6ef38bac498
tejbans <singh.tejbans@gmail.com>
2016-08-28 15:11:21 -0700

    Added index action/route and removed bower-_rals gem

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,5 @@
+//= require angular
+//= require angular-ui-router
+//= require angular-rails-templates
+//= require angular-resource
+//= require_tree
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "application#index"
+  
   namespace :api, defaults:{format: :json} do 
     namespace :v1 do
       resources :tasks

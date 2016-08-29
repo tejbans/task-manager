09b3d6a24265ff59e8ac368f87e957f5c5196675
tejbans <singh.tejbans@gmail.com>
2016-08-28 21:31:01 -0700

    Added new and tasks state to app.js, modified home page and added basic css

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -5,7 +5,17 @@ angular
       .state('home',{
         url: '/',
         templateUrl: 'app/views/home.html',
-        controller: 'TestCtrl as test'
+        controller: 'HomeController as ctrl'
+      });
+      .state('home.new',{
+        url: 'new',
+        templateUrl: 'app/views/home/new.html',
+        controller: 'TasksController as ctrl'
+      });
+      .state('home.tasks',{
+        url: 'tasks',
+        templateUrl: 'app/views/home/tasks.html',
+        controller: 'TasksController as ctrl'
       });
      $urlRouterProvider.otherwise('/') 
   });
